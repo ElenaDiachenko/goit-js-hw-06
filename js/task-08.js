4c6655b6e75a053056971843cef80c3ce29c6c38
ElenaDiachenko <96205320+ElenaDiachenko@users.noreply.github.com>
2022-05-23 18:52:24 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
 
+const formRef = document.querySelector('.login-form');
+
+formRef.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+    const mail = formElements.email.value;
+    const password = formElements.password.value;
+
+    if (mail === '' || password === '') {
+        alert('Для отправки формы все поля должны быть заполнены'); 
+    } else {
+        const formData = {
+        mail,
+        password,
+        }
+        console.log(formData);
+       event.currentTarget.reset() 
+    }
+   
+}   
